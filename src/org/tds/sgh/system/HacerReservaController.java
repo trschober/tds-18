e9b95d2a5bf184d73e0870f51b82fdf626d2f8bc
Tomás Riveros <trschober@gmail.com>
2018-08-04 11:50:25 -0400

    HRC
    hrc

--- src/org/tds/sgh/system/HacerReservaController.java
+++ src/org/tds/sgh/system/HacerReservaController.java
@@ -3,12 +3,26 @@ package org.tds.sgh.system;
 import java.util.GregorianCalendar;
 import java.util.Set;
 
+import org.tds.sgh.business.CadenaHotelera;
 import org.tds.sgh.dtos.ClienteDTO;
+import org.tds.sgh.dtos.DTO;
 import org.tds.sgh.dtos.HotelDTO;
 import org.tds.sgh.dtos.ReservaDTO;
 
 public class HacerReservaController implements IHacerReservaController {
 
+	private final DTO DTO = org.tds.sgh.dtos.DTO.getInstance();
+	
+	// --------------------------------------------------------------------------------------------
+	
+	private CadenaHotelera cadenaHotelera;
+	
+	// --------------------------------------------------------------------------------------------
+	
+	public HacerReservaController(CadenaHotelera cadenaHotelera)
+	{
+		this.cadenaHotelera = cadenaHotelera;
+	}
 	@Override
 	public Set<ClienteDTO> buscarCliente(String patronNombreCliente) {
 		// TODO Auto-generated method stub
