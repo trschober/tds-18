c487af25a72acc50e9f0c12b904c573b9b876f8d
FreddieLagosNavarrete <41975686+FreddieLagosNavarrete@users.noreply.github.com>
2018-08-04 11:23:00 -0400

    sdf
    sdfsd

--- src/org/tds/sgh/business/Reserva.java
+++ src/org/tds/sgh/business/Reserva.java
@@ -19,23 +19,19 @@ public class Reserva {
 	public String getCodigo() {
 		return codigo;
 	}
-
-
 	public void setCodigo(String codigo) {
 		this.codigo = codigo;
-	} 
-	
+	} 	
 	public Reserva(String _Codigo , GregorianCalendar _fechaInicio, GregorianCalendar _fechaFin)
 	{
 		this.codigo =_Codigo;
 		this.fechaInicio=_fechaInicio;
 		this.fechaFin= _fechaFin;
 		
-	}
-	
+	}	
 	public Habitacion getHabitacion(Integer CodigoHabitacion) throws Exception
 	{
-		Habitacion habitacion = this.getHabitacion.get(CodigoHabitacion);
+		Habitacion habitacion = this.habitacion.get(CodigoHabitacion);
 		
 		if (habitacion == null)
 		{
@@ -43,6 +39,54 @@ public class Reserva {
 		}
 		
 		return Habitacion;
+	}
+	public GregorianCalendar getFechaFin() {
+		return fechaFin;
+	}
+	public void setFechaFin(GregorianCalendar fechaFin) {
+		this.fechaFin = fechaFin;
+	}
+	public GregorianCalendar getFechaInicio() {
+		return fechaInicio;
+	}
+	public void setFechaInicio(GregorianCalendar fechaInicio) {
+		this.fechaInicio = fechaInicio;
+	}
+	public Boolean getModificacionPorHuesped() {
+		return modificacionPorHuesped;
+	}
+	public void setModificacionPorHuesped(Boolean modificacionPorHuesped) {
+		this.modificacionPorHuesped = modificacionPorHuesped;
+	}
+	public EstadoReserva getEstado() {
+		return estado;
+	}
+	public void setEstado(EstadoReserva estado) {
+		this.estado = estado;
+	}
+	public Map<String, Habitacion> getHabitaciones() {
+		return habitaciones;
+	}
+	public void setHabitaciones(Map<String, Habitacion> habitaciones) {
+		this.habitaciones = habitaciones;
+	}
+	public Map<String, Huesped> getHuespedes() {
+		return huespedes;
+	}
+	public void setHuespedes(Map<String, Huesped> huespedes) {
+		this.huespedes = huespedes;
+	}
+	public Hotel getHotel() {
+		return hotel;
+	}
+	public void setHotel(Hotel hotel) {
+		this.hotel = hotel;
+	}
+	public Cliente getCliente() {
+		return cliente;
+	}
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
 	}
 	
 	
