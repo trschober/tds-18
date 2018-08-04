e27fd6eb8c9e9ab06e18636232f407bab7ffa195
Cristian K. Cárdenas <ccardensa@outlook.com>
2018-08-04 12:08:38 -0400

    nuevo tomar reserva controller

--- src/org/tds/sgh/system/TomarReservaController.java
+++ src/org/tds/sgh/system/TomarReservaController.java
@@ -1,5 +1,89 @@
 package org.tds.sgh.system;
 
-public class TomarReservaController {
+import java.util.GregorianCalendar;
+import java.util.Set;
+
+import org.tds.sgh.dtos.ClienteDTO;
+import org.tds.sgh.dtos.HotelDTO;
+import org.tds.sgh.dtos.ReservaDTO;
+
+public class TomarReservaController implements ITomarReservaController {
+
+	@Override
+	public Set<ReservaDTO> buscarReservasDelCliente() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<ClienteDTO> buscarCliente(String patronNombreCliente) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ClienteDTO seleccionarCliente(String rut) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean confirmarDisponibilidad(String nombreHotel, String nombreTipoHabitacion,
+			GregorianCalendar fechaInicio, GregorianCalendar fechaFin) throws Exception {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public ReservaDTO registrarReserva(String nombreHotel, String nombreTipoHabitacion, GregorianCalendar fechaInicio,
+			GregorianCalendar fechaFin, boolean modificablePorHuesped) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<HotelDTO> sugerirAlternativas(String pais, String nombreTipoHabitacion, GregorianCalendar fechaInicio,
+			GregorianCalendar fechaFin) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ClienteDTO registrarCliente(String rut, String nombre, String direccion, String telefono, String mail)
+			throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ReservaDTO modificarReserva(String nombreHotel, String nombreTipoHabitacion, GregorianCalendar fechaInicio,
+			GregorianCalendar fechaFin, boolean modificablePorHuesped) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<ReservaDTO> buscarReservasPendientes(String nombreHotel) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ReservaDTO seleccionarReserva(long codigoReserva) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ReservaDTO registrarHuesped(String nombre, String documento) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ReservaDTO tomarReserva() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
