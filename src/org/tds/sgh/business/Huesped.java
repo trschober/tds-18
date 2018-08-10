50f1597b192d0f28d21ba24b159fcc3ded272667
Tomás Riveros <trschober@gmail.com>
2018-08-10 00:35:03 -0400

    cleanup
    cleanup

--- src/org/tds/sgh/business/Huesped.java
+++ src/org/tds/sgh/business/Huesped.java
@@ -1,10 +1,5 @@
 package org.tds.sgh.business;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-
 public class Huesped {
 
 	private String nombre;
